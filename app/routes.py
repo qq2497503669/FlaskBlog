5b6f3a997cae9922e96aaeb4ea3d7ddab4a4d4bd
qq2497503669 <2497503669@qq.com>
2021-02-06 22:26:09 +0800

    frist commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app
+from app import app, forms
 
 
 @app.route('/')
@@ -8,11 +8,17 @@ def index():
     user = {'username': '李芳华'}
     title = '登录'
     posts = [
-        {'author': {'username':'张飞'},
+        {'author': {'username': '张飞'},
          'body': '测试测试测试测试'
          },
-        {'author': {'username':'赵云'},
+        {'author': {'username': '赵云'},
          'body': '测试测试测试测试'
          }
     ]
-    return render_template('index.html', user=user, title=title,posts=posts)
+    return render_template('index.html', user=user, title=title, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = forms.LoginForm(prefix='a')
+    return render_template('login.html', title='登录',form=form)
